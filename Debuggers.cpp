49a628e2cc7d080099cdcb12b505ed345c22da47
ather <shehzadaathertanveer@gmail.com>
2026-04-22 14:43:41 +0500

    Connecting VS Code to Git

---
+++ Debuggers.cpp
@@ -0,0 +1,19 @@
+/* This is the final project of team Debuggers for Programming 
+Fundamentals. The team members are Abdullah Arif Roll# BITF25A012,
+Shehzada Ather Roll# BITF25A047, Abdul Moiz Roll# BITF25A059
+*/
+
+//THIS IS STOCK MANAGEMENT AND BILLING PROGRAM WHICH WILL HELP IN EDITING, 
+//MAINTAINING AND CHECKING STOCK AND WILL ALSO GENERATE RECIEPT
+
+#include<iostream>
+#include<String>
+#include<fstream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    `
+}
