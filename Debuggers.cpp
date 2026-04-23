3710fd91b78ffd07f3ba605cb64afe48d8ca5b03
ather <shehzadaathertanveer@gmail.com>
2026-04-23 14:52:57 +0500

    added stuff

--- Debuggers.cpp
+++ Debuggers.cpp
@@ -15,5 +15,32 @@ using namespace std;
 
 int main()
 {
-    
+    int choice1=0,choiceAdmin=0,choiceUser=0;
+
+    cout<<"Welcome to Stock Management and Billing Program!"<<endl;
+    cout<<"Please select your role:"<<endl;
+    cout<<"0. Exit"<<endl;
+    cout<<"1. Admin"<<endl;
+    cout<<"2. User"<<endl;
+    cout<<"Enter choice: ";
+    cin>>choice1;
+
+    while(choice1>2||choice1<0)
+    {
+        cout<<"INVALID INPUT! \a "<<endl<<endl;
+        cout<<"Please select your role:"<<endl;
+        cout<<"0. Exit"<<endl;
+        cout<<"1. Admin"<<endl;
+        cout<<"2. User"<<endl;
+        cout<<"Enter choice: ";
+        cin>>choice1;
+        cout<<endl;
+    }
+    if (choice1==2)
+    {
+        cout<<"Welcome User!"<<endl;
+
+
+    }
+   
 }
---
+++ stockFileOpenning.cpp
@@ -0,0 +1,15 @@
+ #include<iostream>
+#include<String>
+#include<fstream>
+#include<iomanip>
+
+using namespace std;
+
+int inputStockfile()
+{
+ string fileItemName;
+ int fileItemCode,fileItemPrice,ItemCount;
+  ofstream fout;
+  ifstream fin;
+
+}
