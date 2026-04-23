e9c53e3bee6da1cc6a905c1c78c69bcaf1485add
ather <shehzadaathertanveer@gmail.com>
2026-04-23 21:21:13 +0500

    added file reading functions

--- Debuggers.cpp
+++ Debuggers.cpp
@@ -13,6 +13,9 @@ Shehzada Ather Roll# BITF25A047, Abdul Moiz Roll# BITF25A059
 
 using namespace std;
 
+int StockFileCount();
+void inputsFromstockFile(int[], string[], int[], int[], int);
+
 int main()
 {
     int choice1=0,choiceAdmin=0,choiceUser=0;
@@ -43,4 +46,54 @@ int main()
 
     }
    
+}
+
+int StockFileCount()
+{
+  string fileItemName;
+  int fileItemCode=0,fileItemPrice=0,fileItemCount=0,fileItemQuanity=0;
+  ofstream fout;
+  ifstream fin;
+  fin.open("stock.txt");
+  if (!fin.fail())
+  {
+     while(fin>>fileItemCode)
+      {
+        getline(fin,fileItemName);
+        fin>>fileItemQuanity;
+        fin>>fileItemPrice;
+        fileItemCount++;
+        fin>>fileItemCode;
+      }
+
+     fin.close();
+  }
+  else
+  {
+    cout<<endl;
+    cout<<"Error!\aCould not open stock.txt file"<<endl;
+  }
+   return fileItemCount;
+}
+
+void inputsFromstockFile(int fileItemCodes[],string fileitemName[],int fileItemQuantity[],int fileItemprice[],int fileItemCount)
+{
+   ifstream fin;
+  fin.open("stock.txt");
+  if(!fin.fail())
+  {
+    for(int i=0;i<fileItemCount;i++)
+    {
+      fin >> fileItemCodes[i];
+      getline(fin,fileitemName[i]);
+      fin >> fileItemQuantity[i];
+      fin >> fileItemprice[i];
+    }
+    fin.close();
+  }
+   else
+  {
+    cout<<endl;
+    cout<<"Error!\aCould not open stock.txt file"<<endl;
+  }
 }
--- stockFileOpenning.cpp
+++ stockFileOpenning.cpp
@@ -1,15 +1,56 @@
- #include<iostream>
+#include<iostream>
 #include<String>
 #include<fstream>
 #include<iomanip>
 
 using namespace std;
 
-int inputStockfile()
+int StockFileCount()
 {
- string fileItemName;
- int fileItemCode,fileItemPrice,ItemCount;
+  string fileItemName;
+  int fileItemCode=0,fileItemPrice=0,fileItemCount=0,fileItemQuanity=0;
   ofstream fout;
   ifstream fin;
+  fin.open("stock.txt");
+  if (!fin.fail())
+  {
+     while(fin>>fileItemCode)
+      {
+        getline(fin,fileItemName);
+        fin>>fileItemQuanity;
+        fin>>fileItemPrice;
+        fileItemCount++;
+        fin>>fileItemCode;
+      }
 
+     fin.close();
+  }
+  else
+  {
+    cout<<endl;
+    cout<<"Error!\aCould not open stock.txt file"<<endl;
+  }
+   return fileItemCount;
+}
+
+void inputsFromstockFile(int fileItemCodes[],string fileitemName[],int fileItemQuantity[],int fileItemprice[],int fileItemCount)
+{
+   ifstream fin;
+  fin.open("stock.txt");
+  if(!fin.fail())
+  {
+    for(int i=0;i<fileItemCount;i++)
+    {
+      fin >> fileItemCodes[i];
+      getline(fin,fileitemName[i]);
+      fin >> fileItemQuantity[i];
+      fin >> fileItemprice[i];
+    }
+    fin.close();
+  }
+   else
+  {
+    cout<<endl;
+    cout<<"Error!\aCould not open stock.txt file"<<endl;
+  }
 }
